f1f4126d99d77e55b1cd5c112121c0f8881ef7a0
Alec Castro <alecb.castro@gmail.com>
2022-10-23 21:45:44 -0400

    nll

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {createOrder} from "./api"
+import {createOrder} from "/api"
 
 
 const Checkout = ({ cart, user, setCart }) => {
