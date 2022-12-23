8b6b9d2b182335d33e47b57eb5d2f1d681101b1a
Alec Castro <alecb.castro@gmail.com>
2022-12-23 03:00:17 -0500

    server setup

--- db/client.js
+++ db/client.js
@@ -1,7 +1,8 @@
 // build and export your unconnected client here
 
 const { Client } = require('pg');
-const DB_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/burns_webstore'
+// const DB_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/burns_webstore';
+const DB_URL = '35.245.212.150';
 const client = new Client(DB_URL);
 
 module.exports = {
