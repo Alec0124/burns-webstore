8e3b1e07d3bf5b1e363e035eeb67a0655c4b3ba6
Alec Castro <alecb.castro@gmail.com>
2023-01-22 20:08:15 -0500

    backend update

--- db/client.js
+++ db/client.js
@@ -2,11 +2,18 @@
 
 const { Client } = require('pg');
 // const DB_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/burns_webstore';
+const DB_NAME = "burns_webstore";
+const DB_HOST = "burns-webstore-2.cs2bajfptwuz.us-east-1.rds.amazonaws.com";
+const DB_USER = "postgres";
+const DB_PASSWORD = "cT9kqTlH8NHjpFgETYSI";
+const DB_PORT = 5432;
+
 const client = new Client({
-    host: '35.245.212.150',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
   })
 
 module.exports = {
